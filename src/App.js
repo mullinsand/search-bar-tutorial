2b8fdf2d07ccc1f11ad99d2fe99724090c74a036
Andrew Mullins <mullinsand15@gmail.com>
2023-02-17 11:59:16 -0700

    map over all data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-import Data from "../mock-data.json"
+import Data from "./mock-data.json"
 
 function App() {
   return (
     <div className="App">
       <input placeholder="Enter Post Title"/>
+      {
+        Data.map((post) => (
+          <div className="box" key={post.id}>
+            <p>{post.name}</p>
+            <p>{post.category}</p>
+            <p>{post.price}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
