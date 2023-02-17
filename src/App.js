4df08bad30bbebc2e2f39b8b1c7eed45130b88c6
Andrew Mullins <mullinsand15@gmail.com>
2023-02-17 12:44:03 -0700

    basic filter and map search bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Data from "./mock-data.json"
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [query, setQuery] = useState("")
   return (
     <div className="App">
-      <input placeholder="Enter Post Title"/>
+      <input placeholder="Enter Item Name" onChange={event => setQuery(event.target.value) }/>
       {
-        Data.map((post) => (
-          <div className="box" key={post.id}>
+        Data.filter(post => {
+          if (query === '') {
+            return post;
+          } else if (post.name.toLowerCase().includes(query.toLowerCase())) {
+            return post;
+          }
+        }).map((post, index) => (
+          <div className="box" key={index}>
             <p>{post.name}</p>
+            <img src={post.image} alt={post.name} width="80" height="80"/>
             <p>{post.category}</p>
             <p>{post.price}</p>
           </div>
